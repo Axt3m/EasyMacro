afcd5de3d4d73cab23188e389162faba7b891cb4
Axt3m <angela.terao@gmail.com>
2023-02-22 16:23:43 +0100

    Initial Commit

---
+++ EasyMacro_v1/PushedViewController.swift
@@ -0,0 +1,74 @@
+//
+//  PushedViewController.swift
+//  EasyMacro_v1
+//
+//  Created by Angela Terao on 22/02/2023.
+//
+
+import UIKit
+import SnapKit
+
+class PushedViewController: UIViewController {
+    
+    // MARK: - Init
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        configureViews()
+        configureLayout()
+    }
+    
+    private func configureViews() {
+        view.backgroundColor = .white
+        
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(label1)
+        stackView.addArrangedSubview(label2)
+        stackView.addArrangedSubview(label3)
+    }
+    
+    private func configureLayout() {
+        stackView.snp.makeConstraints{
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Subviews
+    
+    private lazy var stackView: UIStackView = {
+        $0.axis = .vertical
+        $0.spacing = 8
+        return $0
+    }(UIStackView())
+    
+    private lazy var label1: UILabel = {
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .blue
+        $0.text = "Coucou"
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+    
+    private lazy var label2: UILabel = {
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .red
+        $0.text = "Loul"
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+    
+    private lazy var label3: UILabel = {
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .yellow
+        $0.text = "Ava?"
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+    
+
+
+
+}
+
---
+++ EasyMacro_v1/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  EasyMacro_v1
+//
+//  Created by Angela Terao on 20/02/2023.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Init
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        configureViews()
+        configureLayout()
+    }
+    
+    private func configureViews() {
+        view.backgroundColor = .white
+
+        view.addSubview(exampleBlueView)
+        exampleBlueView.addSubview(pushButton)
+        exampleBlueView.addSubview(presentButton)
+    }
+
+    private func configureLayout() {
+        pushButton.snp.makeConstraints{
+            $0.center.equalToSuperview()
+        }
+        
+        presentButton.snp.makeConstraints{
+            $0.top.equalTo(pushButton.snp.bottom).offset(8)
+            $0.centerX.equalTo(pushButton.snp.centerX)
+        }
+
+        exampleBlueView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    // MARK: - Subviews
+
+    private lazy var pushButton: UIButton = {
+        $0.setTitle("Push Button", for: .normal)
+        $0.addTarget(self, action: #selector(pushAction), for: .touchUpInside)
+        return $0
+    }(UIButton())
+    
+    private lazy var presentButton: UIButton = {
+        $0.setTitle("Present Button", for: .normal)
+        $0.addTarget(self, action: #selector(presentAction), for: .touchUpInside)
+        return $0
+    }(UIButton())
+
+
+    private lazy var exampleBlueView: UIView = {
+        $0.backgroundColor = .blue.withAlphaComponent(0.5)
+        return $0
+    }(UIView())
+
+    // MARK: - Actions
+
+    @objc private func pushAction() {
+        let controller = PushedViewController()
+
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    @objc private func presentAction() {
+        let controller = PushedViewController()
+        present(controller, animated: true)
+    }
+
+
+}
+
